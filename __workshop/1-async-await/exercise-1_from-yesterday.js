0af6adb611954d1dc062a5f71a002c3cb3fafccd
Shai Gilboa <shizel@gmail.com>
2020-02-25 12:18:58 -0500

    excersize-2

--- __workshop/1-async-await/exercise-1_from-yesterday.js
+++ __workshop/1-async-await/exercise-1_from-yesterday.js
@@ -7,6 +7,17 @@ const request = require('request-promise');
 // Returns the current position of the ISS
 const getIssPosition = async () => {
     // add code here (include a try/catch as well)
+    try {
+        let issPos = await request('http://api.open-notify.org/iss-now.json')
+        issPos = JSON.parse(issPos)
+        issPos = issPos['iss_position'];
+        const data = {
+            lat: issPos.latitude,
+            lng: issPos.longitude
+        }
+        console.log(data)
+        return data
+    } catch(err) {return console.log(err)}
 }
 
 getIssPosition();
--- __workshop/1-async-await/exericse-2.js
+++ __workshop/1-async-await/exericse-2.js
@@ -1,2 +1,29 @@
 // Exercise 2
 // ----------
+const doubleAfter2Seconds = async (num) => {
+    try {
+    return new Promise (resolve => {
+        setTimeout(() => {
+            resolve(num*2)
+        }, 2000);
+    })
+    } catch (err) {console.log(err)}
+}
+
+const addPromise = async (num) => {
+        try {
+            console.log(num)
+            const a = await doubleAfter2Seconds(10);
+            console.log(a)
+            const b = await doubleAfter2Seconds(20);
+            console.log(b)
+            const c = await doubleAfter2Seconds(30);
+            console.log(c)
+            const sum = num + a + b + c;
+            console.log(sum)
+            return new Promise (resolve => {resolve(sum)});
+        } catch(err) {console.log(err)}
+    
+}
+
+addPromise(10);
